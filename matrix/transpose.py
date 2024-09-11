52ee847e299334f88c456ac04e0f9f0f67547a73
gmajuria <gmajuria@gmail.com>
2024-09-11 12:17:42 -0300

    Agrega funcion transpose

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,8 @@
 def transpose(matrix):
-    pass
+    new_matrix = []
+    for column in range(len(matrix[0])):
+        vector = []
+        for row in range(len(matrix)):
+            vector.append(matrix[row][column])
+        new_matrix.append(vector)
+    return new_matrix
