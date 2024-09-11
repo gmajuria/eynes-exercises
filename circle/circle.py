78824eb4459070158ea447f6a9170744a7467fd0
gmajuria <gmajuria@gmail.com>
2024-09-11 10:42:48 -0300

    Agrega metodos init, get_radius, set_radius y get_area en circle

--- circle/circle.py
+++ circle/circle.py
@@ -1,4 +1,16 @@
 import math
 
 class Circle():
-    pass
+    def __init__(self, radius):
+        self.set_radius(radius)
+
+    def get_radius(self):
+        return self.radius
+
+    def set_radius(self, radius):
+        if radius <= 0:
+            raise ValueError('El radio del circulo debe ser mayor a 0')
+        self.radius = radius
+
+    def get_area(self):
+        return math.pi * self.radius ** 2
